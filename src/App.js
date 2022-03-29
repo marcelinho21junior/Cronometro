7879b7e7ea95b8f054f88ecbde3a9cb0f18c86c0
github.com/marcelinho21junior <marcelo.belitardo@fatec.sp.gov.br>
2022-03-29 12:19:49 -0300

    commit ocntinue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 //components
@@ -6,11 +6,14 @@ import DisplayComponents from './components/displayComponents';
 import BtnComponents from './components/btnComponents';
 
 function App() {
+
+  const[time,setTime] = useState({ms:0 , s:0, m:0, h:0})
+
   return (
     <div className='main-section'>
       <div className='clock-holder'>
         <div className='stopwatch'>
-          <DisplayComponents />
+          <DisplayComponents time={time}/>
           <BtnComponents />
         </div>
       </div>
